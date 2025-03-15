5f29c2c731fe4cbb905df5002c7feb336c5968ff
Quang <qth9368@rit.edu>
2025-03-15 12:11:28 -0400

    grayed out hours/day inputs when date range is shown

--- src/App.js
+++ src/App.js
@@ -115,13 +115,13 @@ function App() {
               <path d="M6.76 4.84l-1.8-1.79L3.55 4.46l1.79 1.8 1.42-1.42zm10.48 0l1.8-1.79-1.42-1.42-1.79 1.79 1.41 1.42zM12 4V0h-2v4h2zm0 20v-4h-2v4h2zm7-12h4v-2h-4v2zm-20 0h4v-2H-1v2zm16.24 7.16l1.8 1.79 1.41-1.41-1.79-1.8-1.42 1.42zm-10.48 0L4.55 17.5l1.41-1.41 1.79 1.79-1.42 1.42zM12 6a6 6 0 100 12 6 6 0 000-12z"/>
             </svg>
           ) : (
-            <svg xmlns="http://www.w3.org/2000/svg" fill="#484742" width="24" height="24" viewBox="0 0 24 24">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="#FFC107" width="24" height="24" viewBox="0 0 24 24">
               <path d="M9.37 5.51A7.5 7.5 0 0116.49 12 7.5 7.5 0 019.37 18.49 7.5 7.5 0 019.37 5.51zm0-2.51c-1.02 0-2.01.16-2.93.47A9 9 0 1018.47 9.3c0-.07-.01-.13-.01-.2a7.5 7.5 0 00-7.5-7.5z"/>
             </svg>
           )}
         </button>
       </header>
-      <h1>Moolah</h1>
+      <h1>Job Income Calculator</h1>
       <form onSubmit={handleSubmit} className="calc-form">
         {jobs.map((job, index) => (
           <div key={index} className="job-card">
